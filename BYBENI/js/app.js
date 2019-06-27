c736f292d9c50b949a09cdb51733361a50a95668
Sany Chernizon <32211613+sanychernizon@users.noreply.github.com>
2019-06-26 21:40:41 -0300

    Cart Done

--- BYBENI/js/app.js
+++ BYBENI/js/app.js
@@ -7,3 +7,22 @@ function toggleNav() {
     mainMobile.classList.toggle('main-on');
     menuBurger.classList.toggle('is-active');
 }
+
+function openCart() {
+    let cart = document.querySelector('.cart-container');
+    let overlay = document.querySelector('.overlay');
+    
+    cart.classList.add('cart-open')
+    overlay.style.display = 'initial'
+
+    overlay.addEventListener('click', closeCart)
+}
+
+function closeCart() {
+    let cart = document.querySelector('.cart-container');
+    let overlay = document.querySelector('.overlay');
+    
+
+    cart.classList.remove('cart-open')
+    overlay.style.display = 'none'
+}
